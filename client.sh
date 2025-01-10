5d07a8b4e83a434f19dc8f528f9a036f51a1a3cb
Chbmleao <caikleao@gmail.com>
2025-01-10 18:15:30 -0300

    feat: change output file extension to .json

--- client.sh
+++ client.sh
@@ -1,5 +1,5 @@
 wget --server-response \
-    --output-document response.out \
+    --output-document response.json \
     --header='Content-Type: application/json' \
     --post-data '{"songs": ["HUMBLE.", "DNA.", "goosebumps"]}' \
     http://10.43.23.218:18485/api/recommend
